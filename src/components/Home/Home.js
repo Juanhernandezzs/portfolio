2e1265897a7ba27aa20c6b2a4c0ad9cd67792b78
Juanhernandezzs <juanmhdz99@gmail.com>
2022-01-18 23:14:49 -0300

    Added spanish resume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './Home.css'
-import resume from '../../resume/Juan-Hernandez-Resume-EN.pdf'
+import resumeEN from '../../resume/Juan-Hernandez-Resume-EN.pdf'
+import resumeES from '../../resume/Juan-Hernandez-Resume-ES.pdf'
 import { LanguageContext } from '../../LanguageContext';
 import text from './text.js'
 
@@ -8,12 +9,13 @@ import text from './text.js'
 function Home() {
     const { Lang } = useContext(LanguageContext);
     const t = text[Lang];
+    const currentLang = Lang
 
     return (
         <div className='intro'>
             <h1 className='home'>Juan Hernández</h1>
             <p className='home__subtitle'>{t.subtitle}</p>
-            <a className='downloadbtn' href={resume}>{t.download}</a>
+            <a className='downloadbtn' href={currentLang === 'es' ? resumeES : resumeEN}>{t.download}</a>
         </div>
     )
 }
